e3ec4f926501f0869866f4c4efcc8370baf0715d
tseijp <_@tsei.jp>
2023-12-04 13:37:38 +0900

    chore: wip

--- i18n/create.ts
+++ i18n/create.ts
@@ -12,37 +12,35 @@ export const createI18N = <
 >(
   options?: I18NConfig<Lng, Ids> & Partial<I18N<Lng, Ids>>,
 ) => {
-  const self = {
-    lng: "ja",
-    map: new Map(),
-    set: new Set(),
-    t(id: Ids) {
-      return self.map.get(self.lng)?.get(id)!;
-    },
-    flush(lng: Lng) {
-      self.lng = lng
-      self.set.forEach((f) => f())
-    },
-    change(lng: Lng) {
-      setLocalStorageLng(lng);
-      i18nSet.forEach((_) => _.flush(lng));
-    },
-    init({ resources, ...other }) {
-      Object.assign(self, other);
-      if (!resources) return;
-      for (let lng in resources) {
-        if (!self.map.has(lng)) self.map.set(lng, new Map());
-        const translation = resources[lng]?.translation;
-        const languageMap = self.map.get(lng);
-        for (const id in translation)
-          languageMap?.set(id, translation[id]);
-      }
-    },
-  } as I18N<Lng, Ids>;;
+  const map = new Map<Lng, Map<Ids, string>>();
+  const set = new Set<() => void>();
+  const t = (id: Ids) => map.get(self.lng)?.get(id)!;
 
+  const flush = (lng: Lng) => {
+    self.lng = lng;
+    set.forEach((f) => f());
+  }
+
+  const change = (lng: Lng) => {
+    setLocalStorageLng(lng);
+    i18nSet.forEach((_) => _.flush(lng));
+  }
+
+  const init = ({ resources, ...other }: I18NConfig<Lng, Ids>) => {
+    Object.assign(self, other);
+    if (!resources) return;
+    for (let lng in resources) {
+      if (!map.has(lng)) map.set(lng, new Map());
+      const translation = resources[lng]?.translation;
+      const languageMap = map.get(lng);
+      for (const id in translation)
+        languageMap?.set(id, translation[id]);
+    }
+  }
+
+  if (options) init(options)
+
+  const self = { lng: 'ja', map, set, t, flush, change, init } as I18N<Lng, Ids>
   i18nSet.add(self as any);
-
-  if (options) self.init(options);
-
   return self;
 };
